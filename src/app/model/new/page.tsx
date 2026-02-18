160adf09d4d56102fd3ca0f481896d79c04d1fc1
Cal <root@ip-172-31-19-93.ec2.internal>
2026-02-18 14:34:16 +0000

    fix: redirect /model/new to /model/new/edit

--- src/app/model/new/page.tsx
+++ src/app/model/new/page.tsx
@@ -1,29 +1,8 @@
-'use client';
+import { redirect } from 'next/navigation';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useModelStore } from '@/stores/model';
-
+/**
+ * /model/new redirects to the editor
+ */
 export default function NewModelPage() {
-  const router = useRouter();
-  const newModel = useModelStore((s) => s.newModel);
-  const model = useModelStore((s) => s.model);
-
-  useEffect(() => {
-    // Create a new model and redirect to editor
-    newModel();
-  }, [newModel]);
-
-  useEffect(() => {
-    // Once model is created, redirect to its edit page
-    if (model) {
-      router.replace(`/model/${model.id}/edit`);
-    }
-  }, [model, router]);
-
-  return (
-    <div className="min-h-screen bg-gray-950 flex items-center justify-center">
-      <div className="text-white text-lg">Creating new model...</div>
-    </div>
-  );
+  redirect('/model/new/edit');
 }
